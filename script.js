c9f8d3590c63ba54b85cc24cf72f80fa6389e3cc
Naiem Foysal <naiemfoysal@Naiems-MacBook-Pro.local>
2023-10-14 00:39:45 +0600

    update-4

--- script.js
+++ script.js
@@ -1,41 +1,39 @@
-// Handle 3 input range fields
-const currentRatingInput = document.getElementById("current-rating");
-const currentRatingValue = document.getElementById("current-rating-value");
-const currentReviewInput = document.getElementById("current-review");
-const currentReviewValue = document.getElementById("current-review-value");
-const desiredRatingInput = document.getElementById("desired-rating");
-const desiredRatingValue = document.getElementById("desired-rating-value");
+// // Handle 3 input range fields
+// const currentRatingInput = document.getElementById("current-rating");
+// const currentRatingValue = document.getElementById("current-rating-value");
+// const currentReviewInput = document.getElementById("current-review");
+// const currentReviewValue = document.getElementById("current-review-value");
+// const desiredRatingInput = document.getElementById("desired-rating");
+// const desiredRatingValue = document.getElementById("desired-rating-value");
 
-// Update the Current Rating value and calculate Desired Rating
-currentRatingInput.addEventListener("input", updateCurrentRating);
-function updateCurrentRating() {
-  const currentRating = parseFloat(currentRatingInput.value);
-  currentRatingValue.textContent = currentRating.toFixed(1);
-  updateDesiredRating(currentRating);
-}
+// // Update the Current Rating value and calculate Desired Rating
+// currentRatingInput.addEventListener("input", updateCurrentRating);
+// function updateCurrentRating() {
+//   const currentRating = parseFloat(currentRatingInput.value);
+//   currentRatingValue.textContent = currentRating.toFixed(1);
+//   updateDesiredRating(currentRating);
+// }
 
-// Update the Current Review value
-currentReviewInput.addEventListener("input", updateCurrentReview);
-function updateCurrentReview() {
-  const currentReview = parseInt(currentReviewInput.value);
-  currentReviewValue.textContent = currentReview;
-}
+// // Update the Current Review value
+// currentReviewInput.addEventListener("input", updateCurrentReview);
+// function updateCurrentReview() {
+//   const currentReview = parseInt(currentReviewInput.value);
+//   currentReviewValue.textContent = currentReview;
+// }
 
-function updateDesiredRating(currentRating) {
-  const desiredRating = currentRating + 0.1;
-  if (desiredRating <= 5.0) {
-    desiredRatingInput.value = desiredRating;
-    desiredRatingValue.textContent = desiredRating.toFixed(1);
-  }
-  else {
-    desiredRatingInput.value = 5.0;
-    desiredRatingValue.textContent = "5.0";
-  }
-}
+// function updateDesiredRating(currentRating) {
+//   const desiredRating = currentRating + 0.1;
+//   if (desiredRating <= 5.0) {
+//     desiredRatingInput.value = desiredRating;
+//     desiredRatingValue.textContent = desiredRating.toFixed(1);
+//   }
+//   else {
+//     desiredRatingInput.value = 5.0;
+//     desiredRatingValue.textContent = "5.0";
+//   }
+// }
 
-updateCurrentRating();
-
-
+// updateCurrentRating();
 
 
 
@@ -48,8 +46,16 @@ toggleSwitch.addEventListener("change", function () {
   if (toggleSwitch.checked) {
     card1.classList.remove("review-cal-toggle-active-card");
     card2.classList.add("review-cal-toggle-active-card");
+    document.querySelector(".toggle-card").style.display = "block";
   } else {
     card1.classList.add("review-cal-toggle-active-card");
     card2.classList.remove("review-cal-toggle-active-card");
+    document.querySelector(".toggle-card").style.display = "none";
   }
 });
+
+
+
+
+
+
